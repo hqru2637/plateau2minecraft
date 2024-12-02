9cb844500632e7989aaf06a371f0bf8c3f83b145
hqru2637 <62416195+hqru2637@users.noreply.github.com>
2024-12-02 11:48:58 +0900

    chore: update command generator

--- index.ts
+++ index.ts
@@ -1,16 +1,19 @@
 // poetry run python -m plateau2minecraft --target "C:\Users\Public\shared\09201_utsunomiya-shi_city_2023_citygml_1_op\udx\bldg\54396753_bldg_6697_op.gml" --output ./result
 
+const isTest = false;
 
 const index = 543967;
 
-// const maps = [
-//   71, 72,
-//   61, 62, 63, 64,
-//       52, 53, 54
-// ]
+const mapsMain = [
+  71, 72,
+  61, 62, 63, 64,
+      52, 53, 54
+];
 
-const maps = [63]
+const mapsTest = [63];
 
+
+const maps = isTest ? mapsTest : mapsMain;
 const bldgPath = 'C:\\Users\\Public\\shared\\09201_utsunomiya-shi_city_2023_citygml_1_op\\udx\\bldg\\';
 const tranPath = 'C:\\Users\\Public\\shared\\09201_utsunomiya-shi_city_2023_citygml_1_op\\udx\\tran\\';
 
